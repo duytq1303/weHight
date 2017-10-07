fe578a77612402e8ccb829c0145c31ace9adc321
DuyTran <duytq@dgroup.co>
2017-10-07 19:36:37 +0700

    Add passport to project

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,11 @@
 
 import compression from 'compression';
 import express from 'express';
-
-import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config';
-import { isProd } from '../shared/util';
+import passport from 'passport';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import {APP_NAME, STATIC_PATH, WEB_PORT} from '../shared/config';
+import {isProd} from '../shared/util';
 import renderApp from './render-app';
 
 const app = express();
@@ -12,14 +14,27 @@ const app = express();
 app.use(compression());
 app.use(STATIC_PATH, express.static('dist'));
 app.use(STATIC_PATH, express.static('public'));
+app.use(session({secret: 'High'}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send(renderApp(APP_NAME));
 });
 
+app.get(
+  '/login',
+  passport.authenticate('local', {failureRedirect: '/login'}),
+  (req, res) => {
+    res.redirect('/');
+  },
+);
 app.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server running on port ${WEB_PORT} ${isProd
-    ? '(production)'
-    : '(development)'}.`);
+  console.log(
+    `Server running on port ${WEB_PORT} ${isProd
+      ? '(production)'
+      : '(development)'}.`,
+  );
 });
